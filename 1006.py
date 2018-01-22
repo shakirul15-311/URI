ef9c1270541d92835ebac54de9dbcb210298e935
Mohammad Shakirul Islam <shakirul15-311@diu.edu.bd>
2018-01-22 23:42:24 +0600

    Create 1006.py

---
+++ 1006.py
@@ -0,0 +1,10 @@
+def printf(format, *values):
+    print(format % values )
+
+A=float(input())
+B=float(input())
+C=float(input())
+
+D = ((A*2)+(B*3)+(C*5))/(2+3+5);
+    
+print("MEDIA = %.1f" %D)
